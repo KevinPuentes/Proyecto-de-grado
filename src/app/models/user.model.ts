fac56e60f300fbe7099706733b422fcf90ed999d
kevinsinho97 <kevinsinho978@hotmail.com>
2021-10-12 20:47:58 -0500

    Cambios Finales

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -5,8 +5,14 @@ export class UsuarioModel {
   birthday: string;
   phone: string;
   address: string;
+  address_espa: string;
+  apellidos_enca: string;
   mail: string;
   document: string;
   fecha_expedicion:string;
+  fecha_nacimiento:string;
+  fecha_asignacion:string;
   tipo_documento:string;
+  name_encargado: string;
+  codigo_encargado: string;
 }
